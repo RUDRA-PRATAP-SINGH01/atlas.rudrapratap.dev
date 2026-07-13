8b635256c9912e8f2afd111b962324ced453e430
RUDRA-PRATAP-SINGH01 <rudrapratap9256@gmail.com>
2026-07-14 00:37:52 +0530

    made it netlify deploy ready

--- src/features/docs/pages/architecture-design/pebbledbArchitectureGraph.js
+++ src/features/docs/pages/architecture-design/pebbledbArchitectureGraph.js
@@ -8,15 +8,3 @@ export {
   getNodeMap,
   getGraphBounds
 };
-
-import { GRAPH_META, nodes, edges, groups, getNodeMap, getGraphBounds } from "./data/graph.js";
-
-export {
-  GRAPH_META,
-  nodes,
-  edges,
-  groups,
-  getNodeMap,
-  getGraphBounds
-};
-
